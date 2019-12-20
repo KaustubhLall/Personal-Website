8b4af95f3ea80bf8439e8943836db8e3e4101b36
kaustubh <klall@ucsd.edu>
2019-12-20 20:39:29 +0530

    Added navbar base, however website looks buggy

--- mywebsite/src/js/navbar.js
+++ mywebsite/src/js/navbar.js
@@ -1,4 +1,38 @@
 /*
 * This file contains the component and functionality related to navigating the website
-*
 */
+
+import React from 'react';
+import Navbar from "react-bootstrap/Navbar";
+import logo from '../logo.svg'
+class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return (
+                <Navbar
+                    bg={'dark'}
+                    variant={'dark'}
+                    sticky={'top'}
+                    collapseOnSelect={true}
+                >
+                    <Navbar.Brand href="#home">
+                        <img
+                            alt=""
+                            src={logo}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />{'Kaustubh Lall'}
+
+                    </Navbar.Brand>
+                </Navbar>
+        );
+    }
+
+}
+
+export default NavBar;
